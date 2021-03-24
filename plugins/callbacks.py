05a06642f33e4fb3fe98ed6f8a5f7943cb3cdc7a
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-03-24 16:49:54 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -44,8 +44,8 @@ async def cb_handler(client, query):
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/{update_channel}"),
-                    InlineKeyboardButton("Support Group", "SUPPORT_GROUP")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/{UPDATE_CHANNEL}"),
+                    InlineKeyboardButton("Support Group",  Config.SUPPORT_GROUP)
                 ]
             ]
         )
